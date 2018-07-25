5c00eb802f135617263c6b5a9d30a12bd7d35fca
rony ginosar <>
2018-07-25 17:31:42 +0300

    rony questions

--- Tama48/min_conflict_algorithm.py
+++ Tama48/min_conflict_algorithm.py
@@ -18,5 +18,19 @@ def min_conflict_solution(buildings_data, all_needs):
     #   additional_heights = evaluate_plan.EvaluatePlan(init_state, new_plan_state, all_needs)
     # ...
 
+
+
+    # iter until max_iter
+    # for i=1 to ____ :
+        # if current_state is a solution of csp then return current_state
+        # we return a solution set of values for the variable
+        # var <-- a randomly chosen variable from the set of conflicted variables CONFLICTED[csp]
+        # value <-- the value v for var that minimizes CONFLICTS(var,v,current_state,csp)
+        # set var = value in current_state
+    # return fail
+
+    # current_state, an initial assignment of values for the variables in the csp
+    # csp, a constraint satisfaction problem: <variables> , <constraints> ,
+
     return additional_heights
 
