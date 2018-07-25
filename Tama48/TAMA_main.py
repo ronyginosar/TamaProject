9de9f7eb819fe1c5fc10e6b73bd69c989e5d9369
ajbruce <arieljbruce@gmail.com>
2018-07-25 19:53:21 +0300

    Needs Part 2

--- Tama48/TAMA_main.py
+++ Tama48/TAMA_main.py
@@ -15,13 +15,19 @@ if __name__ == '__main__':
     buildings_data = ext_data.read_files(dir_path)
 
     is_genetic = 1
-    add_housing_unit = 100
+    add_area = 1000
+    original_area = 0
+    for building_item in buildings_data:
+        if building_item[0]  == 'residential':
+            for building in building_item[1]:
+                original_area += building.area
+
+    print(buildings_data.shape)
 
     # calculate needs
     additional_heights = []
-    building_types = [building[0] for building in buildings_data]
     building_residential = []
-    all_needs = needs.Needs(buildings_data, add_housing_unit)
+    all_needs = needs.Needs(original_area, buildings_data. add_area)
 
     if is_genetic:
         new_plan = genetic_algorithm.find_solution(buildings_data, all_needs)
--- Tama48/evaluate_plan.py
+++ Tama48/evaluate_plan.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 buildings_data: list of tuples, each tuple contains (buildings_type, list of all Building objects in this type)
 additional_height: tuple of (building_id(int), additional_height(meters))
@@ -6,71 +5,131 @@ type_importance_dict:
 """
 
 PERCENTAGE = 100
+SQMETER_PER_PERSON = 22
 # recommended class size
 CLASS_SIZE = 30
 SHUL_AREA = 200
 MIKVE_AREA = 65
 CLASSROOM_AREA = 130
-KINDERGARDEN_NUM_GRADES = 3
+KINDERGARTEN_NUM_GRADES = 3
 PRIMARY_NUM_GRADES = 6
 HS_NUM_GRADES = 4
 
 class Needs(object):
-    def __init__(self, buildings_types,  add_housing_units, avg_family_size = 3.2, age_percentage18 = 2.0, religious_percentage= 20.0):
+    def __init__(self, original_area, buildings_data, add_area,
+                 age_percentage18 = 2.0, religious_percentage= 20.0, elderly_percentage=10,
+                 avg_family_size = 3.2):
+
         self.all_needs_dict = dict()
-        self.buildings_types = buildings_types
-        self.add_housing_units = add_housing_units
-        self.avg_family_size = avg_family_size
+        self.add_population = add_area * SQMETER_PER_PERSON
+        self.original_population = original_area / SQMETER_PER_PERSON
+        self.original_state = original_population
+        self.buildings_data = buildings_data
         self.age_percentage18 = age_percentage18
         self.religious_percentage = religious_percentage
+        self.elderly_percentage = elderly_percentage
+        self.avg_family_size = avg_family_size
 
-        # overall population in average to add given the housing unit we add
-        self.add_population = self.add_housing_units * self.avg_family_size
-        # number of kids in a certain age (for ex. 102 kids in the age of 10 yo)
-        self.grade_size = age_percentage18 * self.add_population / PERCENTAGE
-        self.elderly_size =
-        # number of classes per age to add
-        self.class_rooms_per_age = ceil(self.age_size / CLASS_SIZE)
+        self.building_types = [building[0] for building in buildings_data]
 
-        self.all_building_types = [building[0] for building in self.buildings_types]
-        avg_imp = 1/(len(self.all_building_types)-1) # -1 for not considering the residential
+        avg_imp = 1/(len(self.building_types)-1) # -1 for not considering the residential
         for building in self.buildings_types:
             self.type_importance_dict[building] = avg_imp
 
+
+        # overall population in average to add given the housing unit we add
+        self.add_population = self.add_housing_units * self.avg_family_size
+
+        # number of kids in a certain age (for ex. 102 kids in the age of 10 yo)
+        self.grade_size = age_percentage18 * self.population_increase / PERCENTAGE
+
+        # number of classes per age to add
+        self.class_rooms_per_age = ceil(self.age_size / CLASS_SIZE)
+
+
         # add units of public services
-        self.kindergarden_needs = (self.grade_size * KINDERGARDEN_NUM_GRADES)/ CLASS_SIZE * CLASSROOM_AREA
-        self.primary_needs = (self.grade_size * PRIMARY_NUM_GRADES)/ CLASS_SIZE * CLASSROOM_AREA
-        self.hs_needs = (self.grade_size * HS_NUM_GRADES)/ CLASS_SIZE * CLASSROOM_AREA
-        self.shul_needs =  ((self.add_population*self.religious_percentage/PERCENTAGE)*0.49)*1.1 * SHUL_AREA
+        self.kindergarten_needs = (self.grade_size * KINDERGATDEN_NUM_GRADES)/ CLASS_SIZE * CLASSROOM_AREA
+        self.primaryschool_needs = (self.grade_size * PRIMARY_NUM_GRADES)/ CLASS_SIZE * CLASSROOM_AREA
+        self.highschool_needs = (self.grade_size * HS_NUM_GRADES)/ CLASS_SIZE * CLASSROOM_AREA
+
+        self.synagogue_needs =  ((self.add_population*self.religious_percentage/PERCENTAGE)*0.49)*1.1 * SHUL_AREA
         self.mikve_needs = ((self.add_population*self.religious_percentage/PERCENTAGE)/22.5)*0.007 * MIKVE_AREA
-        self.cc_needs = 0
-        self.ec_needs = 0
-        if(self.add_population <7000):
-            self.police = 100
-        elif(self.add_population <15000):
-            self.police = 500
 
-        elif(self.add_population <40000):
-            self.police = 500
-        elif(self.add_population <100000):
-            self.police = 500
+        previous_police = 0
+        for building_item in self.buildings_data:
+            if building_item[0]  == 'police':
+                for building in building_item[1]:
+                    previous_police += building.area
+
+        if(self.add_population + original_population <7000):
+            self.police_needs = 100 - previous_police
+        elif(self.self.add_population + original_population <15000):
+            self.police_needs = 500 - previous_police
+        elif(self.self.add_population + original_population <40000):
+            self.police_needs = 1500 - previous_police
+        elif(self.self.add_population + original_population <100000):
+            self.police_needs = 3600 - previous_police
+        else:
+            self.police_needs = 4400 - previous_police
 
 
-    #TODO all needs need to be in area
-    def calc_all_needs(self):
+        previous_community = 0
+        for building_item in self.buildings_data:
+            if building_item[0]  == 'community_center':
+                for building in building_item[1]:
+                    previous_community += building.area
+
+        if(self.add_population + original_population <300):
+            self.community_center_needs = 250 - previous_community
+        elif(self.self.add_population + original_population <600):
+            self.community_center_needs = 400 - previous_community
+        else:
+            self.community_center_needs = 750 - previous_community
+
+        self.elderly_center_needs = 0
+
+        previous_health_clinic = 0
+        for building_item in self.buildings_data:
+            if building_item[0]  == 'health_clinic':
+                for building in building_item[1]:
+                    previous_health_clinic += building.area
+
+        if(self.add_population + original_population <300):
+            self.health_clinic_needs = 300 - previous_health_clinic
+        elif(self.self.add_population + original_population <600):
+            self.health_clinic_needs = 500 - previous_health_clinic
+        else:
+            self.health_clinic_needs = 1000 - previous_health_clinic
+
+        self.hospital_needs = 0
+        self.sport_needs = 0
+
+        #all needs are in area (square meters)
         for building in self.buildings_types:
-            if building[0] == 'kindergarden':
-                self.all_needs_dict[building[0]] = self.kindergarden_needs
+            if building[0] == 'kindergarten':
+                self.all_needs_dict[building[0]] = self.kindergarten_needs
             elif building[0] == 'primary_school':
                 self.all_needs_dict[building[0]] = self.primary_needs
             elif building[0] == 'high_school':
                 self.all_needs_dict[building[0]] = self.hs_needs
             elif building[0] == 'synagogue':
-                self.all_needs_dict[building[0]] = self.shul_needs
+                self.all_needs_dict[building[0]] = self.synagogue_needs
+            elif building[0] == 'mikve':
+                self.all_needs_dict[building[0]] = self.mikve_needs
+            elif building[0] == 'police':
+                self.all_needs_dict[building[0]] = self.police_needs
             elif building[0] == 'community_center':
-                self.all_needs_dict[building[0]] = self.cc_needs
+                self.all_needs_dict[building[0]] = self.community_center_needs
             elif building[0] == 'elderly_center':
-                self.all_needs_dict[building[0]] = self.ec_needs
+                self.all_needs_dict[building[0]] = self.elderly_center_needs
+            elif building[0] == 'health_clinic':
+                self.all_needs_dict[building[0]] = self.health_clinic_needs
+            elif building[0] == 'hospital':
+                self.all_needs_dict[building[0]] = self.hospital_needs
+            elif building[0] == 'sport':
+                self.all_needs_dict[building[0]] = self.sport_needs
+
+
 
     # TODO calc for genetic iterations..
     # def calc_plan_evaluation(additional_height):
@@ -80,7 +139,6 @@ class Needs(object):
     #         plan_grade += self.calc_specific_plan_evaluation(building_type)*imp
     #
     #     return plan_grade
-=======
 import needs
 
 class EvaluatePlan(object):
@@ -133,4 +191,3 @@ class EvaluatePlan(object):
         else:
             # maybe to check something else, in this case, it is mandatory to have number of meters per person...
             pass
->>>>>>> 8187f35ab78e029891a8ed0c74c415c8d917ae0e
