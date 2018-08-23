56637e9798bee53f9d9fd2d50e20e18a8e5e31a3
ajbruce <arieljbruce@gmail.com>
2018-08-23 10:22:27 +0300

    added average of 50 people per person type

--- Tama48/evaluate_personal_satisfaction.py
+++ Tama48/evaluate_personal_satisfaction.py
@@ -24,14 +24,21 @@ def evaluate_personal_satisfaction(plan):
 	people.append(p.Person(PARENT, False))
 	people.append(p.Person(ELDERLY,True))
 	personal_satisfaction_raw = []
-	personal_satisfaction = []
 	max_sat = 0
 	for person in people:
-		person.set_residence(residential_buildings[np.random.randint(len(residential_buildings))])
-		curr_sat = round(person.set_satisfaction(),4)
-		personal_satisfaction_raw.append(curr_sat)
-		if(curr_sat>max_sat):
-			max_sat=curr_sat
+		curr_person_type_score =[]
+		for j in range(50):
+			person.set_residence(residential_buildings[np.random.randint(len(residential_buildings))])
+			# curr_sat = round(person.set_satisfaction(),4)
+			curr_sat = person.set_satisfaction()
+			curr_person_type_score.append(curr_sat)
+		curr_person_type_sat = np.mean(curr_person_type_score)
+		personal_satisfaction_raw.append(curr_person_type_sat)
+		
+		if(curr_person_type_sat>max_sat):
+			max_sat=curr_person_type_sat
+
+	personal_satisfaction = []
 	for i in range(len(people)):
 		curr_sat = round(personal_satisfaction_raw[i]/max_sat,4)
 		personal_satisfaction.append((people[i].get_type(), str(people[i].get_religious()), people[i].get_residence(), curr_sat)) 
